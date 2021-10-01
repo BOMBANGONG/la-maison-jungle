b8aa88155e080349e82f1c55ec8ed2a518094102
Cyrille Bomba <c.bombangong@fulll.fr>
2021-10-01 05:15:15 +0200

    Modifying Cart.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Banner from "./components/Banner";
 import Cart from "./components/Cart";
+import ShoppingList from "./components/ShoppingList";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div className="App">
       <Banner />
       <Cart />
+      <ShoppingList />
+      <Footer />
     </div>
   );
 }
