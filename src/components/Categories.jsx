1a98c0cc24d1563b9295360ed6f3d842507f0f20
Cyrille Bomba <c.bombangong@fulll.fr>
2021-10-05 11:25:21 +0200

    Add categories selection and filter

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,23 +1,24 @@
 import "../styles/Categories.css";
+import Button from "./Button";
+import Select from "./Select";
 
 function Categories({ setActiveCategory, categories, activeCategory }) {
+  function handleChange(v) {
+    setActiveCategory(v?.target.value);
+  }
   return (
     <div className="lmj-categories">
-      <select
+      <Select
+        label="Choose category"
+        options={categories}
         value={activeCategory}
-        onChange={(e) => setActiveCategory(e.target.value)}
-        className="lmj-categories-select"
-      >
-        <option value="">---</option>
-        {categories.map((cat) => (
-          <option key={cat} value={cat}>
-            {cat}
-          </option>
-        ))}
-      </select>
-      <button onClick={() => setActiveCategory("")}>Reset</button>
+        onChange={handleChange}
+      />
+      <Button label={"Reset"} onClick={() => setActiveCategory("")} />
     </div>
   );
 }
 
 export default Categories;
+
+// setSelectedCategory
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,32 +1,14 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { Select as UiSelect } from "@ied/ui-select";
 
-const Select = () => {
-  const [compactValue, setCompactValue] = useState("");
+const Select = (props) => {
   return (
     <div>
       <UiSelect
-        id="compact_1"
-        dataTestId="compact_1"
-        compact
-        label="Select your food"
-        options={[
-          {
-            label: "Pizza",
-            value: "2",
-          },
-          {
-            label: "Cookies",
-            value: "1",
-          },
-          {
-            label: "Icecream",
-            value: "3",
-          },
-        ]}
-        onChange={(e) => setCompactValue(e.target.value)}
-        value={compactValue}
-        required
+        label={props.label}
+        options={props.options}
+        onChange={props.onChange}
+        value={props.value}
         color="primary"
       />
     </div>
