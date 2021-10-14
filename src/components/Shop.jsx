d7b22bf3f676c6ce23ad3492fb5e6a6129916c6c
Cyrille Bomba <c.bombangong@fulll.fr>
2021-10-14 10:21:45 +0200

    hook effect on saving item after reloading page

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,21 +2,27 @@ import React from "react";
 import ShoppingList from "./ShoppingList";
 import Description from "./Description";
 import Cart from "./Cart";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Route, useRouteMatch, Switch } from "react-router-dom";
 
 const Shop = () => {
-  const [cart, setCart] = useState([]);
+  // save cart items after refreshing page
+  const savedCart = localStorage.getItem("cart");
+  const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : []);
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
   const { path } = useRouteMatch();
   return (
     <div>
-      <Cart cart={cart} setCart={setCart} />
+      <Cart cart={cart} setCart={updateCart} />
       <Switch>
         <Route path={`${path}/:id`}>
-          <Description setCart={setCart} />
+          <Description setCart={cart} />
         </Route>
         <Route path={`${path}`}>
-          <ShoppingList cart={cart} setCart={setCart} />
+          <ShoppingList cart={cart} setCart={updateCart} />
         </Route>
       </Switch>
     </div>
