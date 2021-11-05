bf568ff40ae0c7c6d4863089567d5d1aa5cfad7c
Cyrille Bomba <c.bombangong@fulll.fr>
2021-11-05 10:52:28 +0100

    fixes

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import UiProgress from "@ied/ui-progress";
 
-const Progress = (props) => {
-  return <UiProgress color="yellow-500" spinner size="xlarge" {...props} />;
+const Progress = () => {
+  return <UiProgress spinner size="xlarge" />;
 };
 
 export default Progress;
